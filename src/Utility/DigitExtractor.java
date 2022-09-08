67e37c7b1aa0620d5c78a233b2295e1b475ad1f7
Lupi1978 <sanjithanupama@gmail.com>
2022-09-08 15:13:03 -0400

    Digit extractor

---
+++ src/Utility/DigitExtractor.java
@@ -0,0 +1,15 @@
+package Utility;
+
+public class DigitExtractor {
+    public static int last(int n) {
+        if(n<0){
+            n = -n; // not mathematically read. Could use math(absolute).
+
+        }
+        return n% 10;
+    }
+    public static void main(String []args) {
+        System.out.println(last(2753) == 3);
+        System.out.println(last(-749) ==9);
+    }
+}
